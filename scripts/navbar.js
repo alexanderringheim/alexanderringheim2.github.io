d89fb2b2e317dccb84079517bc3c670d9ccc3e3d
Qazzyz <Qazzyz@users.noreply.github.com>
2019-01-28 21:53:42 +0100

    resizing #1

---
+++ scripts/navbar.js
@@ -0,0 +1,8 @@
+window.onresize = function() {
+    var elem = document.getElementsByName('body');
+    var navbar = document.getElementById('');
+};
+
+document.getElementById('small-screen-container').onclick = function() {
+    
+};
