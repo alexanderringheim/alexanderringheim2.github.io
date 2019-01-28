1fef7dfa209ac6e8431154c93e3ecac4861c4bff
Qazzyz <Qazzyz@users.noreply.github.com>
2019-01-28 23:32:38 +0100

    almost finished header

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,8 +1,32 @@
-window.onresize = function() {
+window.onresize = function() 
+{
     var elem = document.getElementsByName('body');
     var navbar = document.getElementById('');
 };
 
-document.getElementById('small-screen-container').onclick = function() {
-    
-};
+window.onclick = function(event)
+{
+    if (event.target != document.getElementById('small-screen-container'))
+    {
+        if (event.target != document.getElementById('links-img'))
+        {
+            var elems = document.getElementsByClassName('linksHidden');
+            for (var i = 0; i < elems.length; i++) {
+                var elem = elems[i];
+                elem.style.display = "none";
+            }
+        }
+    }
+}
+
+function showLinks()
+{
+    var elems = document.getElementsByClassName('linksHidden');
+    for (var i = 0; i < elems.length; i++)
+    {
+        var elem = elems[i];
+        elem.style.display = "block";
+    }
+}
+
+
